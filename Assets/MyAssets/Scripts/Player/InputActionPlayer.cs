72534c6418115fff1364c15aad47afd10f994d81
DESKTOP-3ICN3OL\1220AM <fuwakoromochi@gmail.com>
2025-05-26 15:22:10 +0900

    onMoveのみ実装。

--- Assets/MyAssets/Scripts/Player/InputActionPlayer.cs
+++ Assets/MyAssets/Scripts/Player/InputActionPlayer.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class InputActionPlayer : MonoBehaviour
 {
+    [SerializeField]private Animator anim;
     private Vector3 _velocity;
 
     public void OnMove(InputAction.CallbackContext context)
@@ -12,6 +13,12 @@ public class InputActionPlayer : MonoBehaviour
 
         // ˆÚ“®‘¬“x‚ð•ÛŽ
         _velocity = new Vector3(axis.x, 0, axis.y);
+
+        bool isRunning = _velocity.sqrMagnitude > 0.01f;
+        anim.SetBool("isRunning", isRunning);
+
+        if (_velocity != Vector3.zero)
+            transform.rotation = Quaternion.LookRotation(_velocity);
     }
 
     private void Update()
